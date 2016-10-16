f53879072f6f936ae026a6b867517bba54ee4abb
Elvis Nuñez <elvisnunez@me.com>
2016-10-16 16:28:43 +0200

    Update to Swift 3

--- Source/UIViewController+Container.swift
+++ Source/UIViewController+Container.swift
@@ -6,7 +6,7 @@ public extension UIViewController {
      - parameter controller: The controller to be added
      - parameter frame: The frame for the added controller, if the frame is not provided or is `nil` then the parent controller's frame will be used
      */
-    public func addController(controller: UIViewController, frame: CGRect? = nil) {
+    public func add(_ controller: UIViewController, frame: CGRect? = nil) {
         self.addChildViewController(controller)
 
         if let frame = frame {
@@ -14,15 +14,15 @@ public extension UIViewController {
         }
 
         self.view.addSubview(controller.view)
-        controller.didMoveToParentViewController(self)
+        controller.didMove(toParentViewController: self)
     }
 
     /**
      Removes the controller from the view hierarchy safely.
      - parameter controller: The controller to be removed
      */
-    public func removeController(controller: UIViewController) {
-        controller.willMoveToParentViewController(nil)
+    public func remove(_ controller: UIViewController) {
+        controller.willMove(toParentViewController: nil)
         controller.view.removeFromSuperview()
         controller.removeFromParentViewController()
     }
